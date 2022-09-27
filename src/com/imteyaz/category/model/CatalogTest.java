ca14a8fc4b44f21444afdf8944fb5745157d15b2
md-imteyaz <imteyazmd143@gmail.com>
2022-09-27 13:12:32 +0530

    second change

---
+++ src/com/imteyaz/category/model/CatalogTest.java
@@ -0,0 +1,32 @@
+package com.imteyaz.category.model;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+public class CatalogTest {
+    @Test
+    public void testbuildCategories() {
+
+        List<Integer> amount = new ArrayList<Integer>();
+
+        if (amount.isEmpty()) {
+            System.out.println("amount should be credit");
+        } else {
+            System.out.println("Amount credited");
+        }
+
+
+    }
+    @Test
+    public void testQuantity() {
+
+        List<Integer> quantity=new ArrayList<>();
+        if(quantity.contains(0)){
+            System.out.println("Quantity invalid");
+        }
+        }
+    }
+
